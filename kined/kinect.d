8805687f31351826f445d025749df1d7f5a1a7d8
hoge piyo <kointosudesuyo@infoseek.jp>
2014-04-20 12:07:46 +0900

    Add D header declares C wrapper functions.

---
+++ kined/kinect.d
@@ -0,0 +1,89 @@
+module kinect;
+
+extern(C){
+	//data of a pixel
+	struct BGRA{
+		ubyte b,g,r,a;
+	}
+
+	//Wrapping Vector4
+	struct Position{
+		float x,y,z,w;
+	}
+	//Wrapping NUI_COLOR_IMAGE_POINT
+	struct ColorPoint{
+		int x,y;
+	}
+	//Wrapping NUI_DEPTH_IMAGE_POINT
+	struct DepthPoint{
+		float x,y;
+	}
+
+	//Wrapping NUI_SKELETON_TRACKING_STATE
+	enum TrackingState{
+		NOT_TRACKED,
+		INFERRED,
+		TRACKED
+	}
+
+	//extract important members from NUI_SKELETON_DATA
+	struct SkeletonData{
+		Position position;
+		TrackingState state;
+		Position skeleton_positions[20];
+		TrackingState skeleton_states[20];
+	}
+
+	//Wrapping NUI_SKELETON_POSITION_INDEX
+	enum SkeletonPositionIndex
+    {
+		HIP_CENTER,
+		SPINE,
+		SHOULDER_CENTER,
+		HEAD,
+		SHOULDER_LEFT,
+		ELBOW_LEFT,
+		WRIST_LEFT,
+		HAND_LEFT,
+		SHOULDER_RIGHT,
+		ELBOW_RIGHT,
+		WRIST_RIGHT,
+		HAND_RIGHT,
+		HIP_LEFT,
+		KNEE_LEFT,
+		ANKLE_LEFT,
+		FOOT_LEFT,
+		HIP_RIGHT,
+		KNEE_RIGHT,
+		ANKLE_RIGHT,
+		FOOT_RIGHT,
+		COUNT
+    }
+
+	//Initialize Kinect. If Kinect is already initialized, do nothing.
+	void InitializeKinect();
+	//Finalize Kinect. This function must be called at the end of the program.
+	void FinalizeKinect();
+
+	//Get the size of color/depth image.
+	void GetImageSize(int *width,int *height);
+	//Get color image. This function needs ptr can contain width*height elements.
+	void GetColorImage(BGRA *ptr);
+	//Get depth image. This function needs ptr can contain width*height elements.
+	void GetDepthImage(ushort *ptr);
+	//Get depth image. This function needs ptr can contain 6 elements.
+	void GetSkeletons(SkeletonData *ptr);
+
+	//functions as its name
+	void TransformSkeletonPositionToDepthPoint(Position pos,DepthPoint *pt);
+	void TransformDepthPointToColorPoint(DepthPoint dep,ColorPoint *col);
+	void TransformSkeletonPositionToColorPoint(Position pos,ColorPoint *col);
+
+	//helper
+	ushort GetDepthNumber(ushort val){
+		return val >> 3;
+	}
+	ushort GetPlayerIndex(ushort val){
+		return val && 7;
+	}
+}
